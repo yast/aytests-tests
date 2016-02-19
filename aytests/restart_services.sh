25809d5afb167cce8b92405e20ae2e9fc78f42a0
Stefan Schubert <schubi@suse.de>
2016-02-19 12:48:59 +0100

    reducing restarting services test to dbus only

--- aytests/restart_services.sh
+++ aytests/restart_services.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e -x
-# checking if dbus and all wickedd services has not been restarted
-# bnc#944349
+# checking if dbus service has not been restarted
+# bnc#937900
 
-grep "restarting services" /var/log/YaST2/y2log| grep -v 'dbus.service\|wickedd' && echo "AUTOYAST OK"
+grep "restarting services" /var/log/YaST2/y2log| grep -v 'dbus.service' && echo "AUTOYAST OK"
