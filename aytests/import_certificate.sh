ed848f9b17057dd392bcd4207682add41fc3384d
Imobach González Sosa <igonzalezsosa@suse.com>
2016-08-03 10:58:04 +0100

    Avoid globs in zgrep invocation

--- aytests/import_certificate.sh
+++ aytests/import_certificate.sh
@@ -5,5 +5,5 @@ set -e -x
 # Check that the certificate from the registration server is imported.
 # TODO: It would be nice to check that the file is finally copied to
 # the installed system, but it only happens when the system is registered.
-zgrep "Certificate import result: true" /var/log/YaST2/y2log*
+zgrep "Certificate import result: true" /var/log/YaST2/y2log-1.gz
 echo "AUTOYAST OK"
--- aytests/self_update_reg.sh
+++ aytests/self_update_reg.sh
@@ -4,5 +4,5 @@ set -e -x
 
 # FATE#319716 - the installer updates need to be installed into inst-sys
 # check that the URL from the registration server is detected.
-zgrep "Updates for 'SLES' are available at.*static/repos.*" /var/log/YaST2/y2log*
+zgrep "Updates for 'SLES' are available at.*static/repos.*" /var/log/YaST2/y2log-1.gz
 echo "AUTOYAST OK"
