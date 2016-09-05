4aad2051f9456b481f20207ffdb5618a11e2e491
Stefan Schubert <schubi@suse.de>
2016-09-05 14:27:03 +0200

    checking users entry

---
+++ aytests/double_users.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e -x
+zgrep "Found users in profile with equal <username>."\
+ /var/log/YaST2/y2log-1.gz && echo "AUTOYAST OK"
---
+++ aytests/users.rb
@@ -0,0 +1,5 @@
+require "aytests/spec_helper"
+
+describe "SLES 12 Users " do
+  include_examples "test_scripts", "users"
+end
