2d1b3d0992b5883c7f7823107f93ba5e18984bfc
Stefan Schubert <schubi@suse.de>
2016-09-05 14:50:50 +0200

    better syntax

--- aytests/double_users.sh
+++ aytests/double_users.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 set -e -x
-zgrep "Found users in profile with equal <username>."\
- /var/log/YaST2/y2log-1.gz && echo "AUTOYAST OK"
+zgrep "Found users in profile with equal <username>." /var/log/YaST2/y2log-1.gz
+echo "AUTOYAST OK"
--- aytests/users_no_uid.sh
+++ aytests/users_no_uid.sh
@@ -2,7 +2,5 @@
 
 set -e -x
 # Checking if no error has been reported for not defined user UIDs. (bnc#996823)
-DOUBLE_UID=1
-zgrep "Found users in profile with equal <uid>."\
- /var/log/YaST2/y2log-1.gz && DOUBLE_UID=0
-test $DOUBLE_UID -eq 1 && echo "AUTOYAST OK"
+zgrep "Found users in profile with equal <uid>." \
+ /var/log/YaST2/y2log-1.gz || echo "AUTOYAST OK"
