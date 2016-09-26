5d77920c8d988ec2a4198a43e75304ad5facc7ca
Stefan Schubert <schubi@suse.de>
2016-09-26 17:30:40 +0200

    added kdump tests

--- aytests/kdump.sh
+++ aytests/kdump.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -x
+set -e -x
 
 # Check it handles correctly multiple kdump crash_kernel options
 
-grep "crashkernel=72M,low crashkernel=32M,high" /boot/grub2/grub.cfg && echo "AUTOYAST OK"
+grep "crashkernel=72M,low crashkernel=32M,high" /boot/grub2/grub.cfg
+# Check if general section has not been overwritten by proposal. bnc#995750
+grep "<KDUMP_KEEP_OLD_DUMPS>10</KDUMP_KEEP_OLD_DUMPS>" /root/autoinst.xml
+echo "AUTOYAST OK"
