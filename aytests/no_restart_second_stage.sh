5d52bff7826ec3d9d9f182b228177f7a9dc88ff7
Stefan Schubert <schubi@suse.de>
2017-02-06 11:13:41 +0100

    second_stage service checked

--- aytests/no_restart_second_stage.sh
+++ aytests/no_restart_second_stage.sh
@@ -4,4 +4,4 @@ set -e -x
 
 # bug 956043 - YAST second stage must not restarted while activating
 
-grep "Skipping service YaST2-Second-Stage - it's already in desired state" /var/log/YaST2/y2log && echo "AUTOYAST OK"
+grep -E ".*restarting services.*YaST2-Second-Stage" /var/log/YaST2/y2log || echo "AUTOYAST OK"
