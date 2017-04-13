723b993526df5a36cff91eb4b26870c550b14384
Stefan Schubert <schubi@suse.de>
2017-04-13 17:24:10 +0200

    checking if service is enabled

--- aytests/cms.sh
+++ aytests/cms.sh
@@ -9,6 +9,9 @@ grep "dummy" /etc/salt/pki/minion/minion.pub
 # checking if the master has been set
 grep "master: 'linux-addc'" /etc/salt/minion
 
+# checking if the service is enabled
+systemctl is-enabled salt-minion.service
+
 # checking connection to the salt master
 zgrep "salt-call --log-level debug state.highstate" /var/log/YaST2/y2log-1.gz
 zgrep "Reading configuration from /etc/salt/minion" /var/log/YaST2/y2log-1.gz && echo "AUTOYAST OK"
