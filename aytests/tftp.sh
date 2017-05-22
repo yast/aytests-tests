1cd07f13ec0a97f2ec2d4dead1915f61940ecb3a
Stefan Schubert <schubi@suse.de>
2017-05-22 15:07:20 +0200

    fixed for running under sles12-sp3

--- aytests/tftp.sh
+++ aytests/tftp.sh
@@ -2,9 +2,11 @@
 
 set -e -x
 
+HO=`hostname`
+
 echo test >/srv/tftpboot/test.txt
-time tftp localhost -c get test.txt test2.txt || \
-time tftp localhost -c get test.txt test2.txt
+time tftp $HO -c get test.txt test2.txt || \
+time tftp $HO -c get test.txt test2.txt
 
 diff -u /srv/tftpboot/test.txt test2.txt && echo "AUTOYAST OK"
 
