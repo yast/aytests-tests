557c1e1b16bd8c553d4efc0022bd09bc405e6f72
Michal Filka <mfilka@suse.cz>
2017-06-21 10:29:31 +0200

    Improved DNS test according review.

--- aytests/dns_setup.sh
+++ aytests/dns_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e -x
+set -e
 
-fgrep -q 'WRITE_HOSTNAME_TO_HOSTS="no"' /etc/sysconfig/network/dhcp || exit 1
-fgrep -q 'DHCLIENT_SET_HOSTNAME="no"' /etc/sysconfig/network/dhcp || exit 2
-fgrep -q 'vagrant-sles12.vagrantup.com' /etc/HOSTNAME || exit 3
+fgrep -q 'WRITE_HOSTNAME_TO_HOSTS="no"' /etc/sysconfig/network/dhcp
+fgrep -q 'DHCLIENT_SET_HOSTNAME="no"' /etc/sysconfig/network/dhcp
+fgrep -q 'vagrant-sles12.vagrantup.com' /etc/HOSTNAME
 
 echo "AUTOYAST OK"
