63c4b4a555ffcbdd0f0b1fd66e870ee8e7a38e3f
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-11-28 11:20:41 +0000

    Write and export ssh authorized keys for root

--- aytests/authorized_keys.sh
+++ aytests/authorized_keys.sh
@@ -7,6 +7,8 @@ set -e -x
 USER="suse"
 AUTHORIZED_KEYS="/home/$USER/.ssh/authorized_keys"
 PROFILE="/root/autoinst.xml"
+# Check that root ssh authorized keys are written an exported
+ROOT_AUTHORIZED_KEYS="/root/.ssh/authorized_keys"
 
 # Check content
 grep "vagrant@localhost.net" $AUTHORIZED_KEYS
@@ -17,7 +19,19 @@ test $KEYS -eq 1
 STAT=`stat -c '%U %a' $AUTHORIZED_KEYS`
 test "$STAT" == "$USER 600"
 
+# Check root ssh authorized keys content
+grep "root@localhost.net" $ROOT_AUTHORIZED_KEYS
+ROOT_KEYS=`wc -l $ROOT_AUTHORIZED_KEYS | cut -f1 -d " "`
+test $ROOT_KEYS -eq 1
+
+# Check root ssh authorized keys owner and permissios
+STAT=`stat -c '%U %a' $ROOT_AUTHORIZED_KEYS`
+test "$STAT" == "root 600"
+
 # Check that authorized_keys is present in the exported profile
 grep "authorized_keys" $PROFILE
+
+# Check that root ssh authorized keys are exported
+grep "lvjKyz root@localhost.net" $ROOT_AUTHORIZED_KEYS
 
 echo "AUTOYAST OK"
