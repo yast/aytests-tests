98436c59486fd163492f72dede72cea73a9dc531
Imobach González Sosa <igonzalezsosa@suse.com>
2017-12-19 10:02:36 +0000

    Fix chrony service name

--- aytests/chrony.sh
+++ aytests/chrony.sh
@@ -4,6 +4,6 @@ set -e -x
 
 # Check whether the chrony service configured and running (FATE#323432)
 
-systemctl is-active chrony
+systemctl is-active chronyd
 grep "pool cz.pool.ntp.org" /etc/chrony.conf
 echo "AUTOYAST OK"
