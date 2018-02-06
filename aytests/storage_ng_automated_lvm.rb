e860c02655db31067ae17fb8b3c92c736bad048d
Stefan Schubert <schubi@suse.de>
2018-02-06 16:25:11 +0100

    adapted storage_ng_automated_lvm

--- aytests/storage_ng_automated_lvm.rb
+++ aytests/storage_ng_automated_lvm.rb
@@ -3,5 +3,5 @@ require "aytests/spec_helper"
 # This test needs rubygem-aytests (>= 1.0.41)
 
 describe "Storage-ng default disk proposal," do
-  include_examples "test_scripts", "storage_ng_lvm"
+  include_examples "test_scripts", "storage_ng_automated_lvm"
 end
