bf0914999e694fd6fa37bb9b494f790d92facc49
Stefan Schubert <schubi@suse.de>
2018-02-06 13:57:04 +0100

    renamed

--- aytests/sles12_network_without_hosts.rb
+++
@@ -1,6 +0,0 @@
-require "aytests/spec_helper"
-
-describe "SLES 12 network installation without host section" do
-  # Test SLES 12 installation over VNC
-  include_examples "test_scripts", "sles12_network_without_hosts"
-end
