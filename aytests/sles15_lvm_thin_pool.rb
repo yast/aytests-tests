4c4bcd68821035df6f0901b8fa01e29c88514637
Stefan Schubert <schubi@suse.de>
2018-05-15 16:54:36 +0200

    fixed chronyd check

--- aytests/sles15_lvm_thin_pool.rb
+++ aytests/sles15_lvm_thin_pool.rb
@@ -1,5 +1,5 @@
 require "aytests/spec_helper"
-
+# This test is currently not running bnc#1086661
 describe "SLES 15 LVM thin pool checks," do
   include_examples "test_scripts", "sles15_lvm_thin_pool"
 end
